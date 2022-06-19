41e0dd11d592874d5b62abb8280da46cd393b598
RonexLemon <ronexondimu@gmail.com>
2022-06-19 14:03:07 +0300

    initial commit

---
+++ app/src/main/java/com/example/store/storeapplication/StoreApplication.kt
@@ -0,0 +1,8 @@
+package com.example.store.storeapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StoreApplication: Application() {
+}
